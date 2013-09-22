d877253417b681ca7ae7aef4f639099afdfa0fdc
rektide <rektide@voodoowarez.com>
2013-09-22 19:35:02 -0400

    Revamp composition, capture only what is present.

--- ei-el.js
+++ ei-el.js
@@ -4,7 +4,7 @@ var eachline= require("eachline"),
 
 var argv = optimist
   .usage("Usage: $0 [-w x,x,..,x] [-d '\s]'")
-  .options("w", {alias: "words", default: "300"})
+  .options("w", {alias: "words", default: "700"})
   .options("d", {alias: "delim", default: "\\s"})
   .argv;
 
@@ -15,30 +15,32 @@ if(argv.w == "-1")
 	argv.w = "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1"
 
 var wordMatches= []
-wordMatches= argv.w.split(",").map(function(c){
+wordMatches= argv.w.toString().split(",").map(function(c,w){
 	if(!c)
 		throw "Invalid word expression- empty"
 	if(isNaN(c))
 		throw "Invalid word expression- not a number"
-	var expr= ["(",data,"+"],
+	var expr= ["(?:",delim,"*("],
 	  words= parseInt(c)
 	if(words == -1)
 		return 
 	if(words < 1)
 		throw "Must have positive words"
-	for(var i= 1; i< words; ++i){
-		expr.push("(?:",delim,"+",data,"+)?")
+	for(var i= 0; i< words; ++i){
+		expr.push("(?:",i==0?"":(delim+"+"),data,"+)?")
 	}
-	expr.push(")")
+	expr.push("))?")
 	return expr.join("")
 })
 
-var matcher= wordMatches.join(delim+"+"),
+var matcher= wordMatches.join(""),
   expr= new RegExp(matcher),
   started= false
 console.log("EXPR",matcher)
 process.stdout.write("[")
 process.stdin.pipe(eachline(function(line){
+	if(line == "")
+		return ""
 	var buf= [","]
 	if(!started){
 		started= true
@@ -52,7 +54,9 @@ process.stdin.pipe(eachline(function(line){
 	}else{
 		buf.push("[",printIt(res[1]))
 		for(var i= 2; i< res.length; ++i){
-			buf.push(",",printIt(res[i]))
+			var r= res[i]
+			if(r)
+				buf.push(",",printIt(res[i]))
 		}
 		buf.push("]")
 	}
