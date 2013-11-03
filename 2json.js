003e40c60d31aeaf5dab04a705191ce581bb8d64
rektide <rektide@voodoowarez.com>
2013-11-03 00:26:13 -0400

    Use node in #!.

--- 2json.js
+++ 2json.js
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env node
 var eachline= require("eachline"),
   optimist= require("optimist")
 
--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env node
 var optimist= require("optimist")
 
 var argv = optimist
